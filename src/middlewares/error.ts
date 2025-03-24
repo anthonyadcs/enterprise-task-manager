30e9971d635310a0d08f2f0cc4c19d8eaf119c86
anthonyadcs <anthonydacsilva10@gmail.com>
2025-03-24 17:26:50 -0300

    refactor: changes the default json structure when error

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,15 +1,16 @@
-import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import { APIError } from "../handlers/apiError";
 
+export function errorMiddleware(
+	error: Error & Partial<APIError>,
+	request: Request,
+	response: Response,
+	next: NextFunction,
+) {
+	const statusCode = error.statusCode ?? 500;
 
+	const message =
+		error.message && error.message.trim() !== "" ? error.message : "Erro inesperado no servidor";
 
-
-
-
-export function errorMiddleware(error: Error & Partial<APIError>, request: Request, response: Response, next: NextFunction){
-    const statusCode = error.statusCode ?? 500
-
-    const message = error.message && error.message.trim() !== '' ? error.message : "Erro inesperado no servidor"
-
-    response.status(statusCode).json({message})
+	response.status(statusCode).json({ success: false, data: { message } });
 }

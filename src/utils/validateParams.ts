4f2423edf1769a5c8d89b5abeaf23865a7413495
anthonyadcs <anthonydacsilva10@gmail.com>
2025-03-25 17:40:17 -0300

    feat: adds parse method to validateParams class

--- src/utils/validateParams.ts
+++ src/utils/validateParams.ts
@@ -26,6 +26,31 @@ class ValidateParams {
 			throw new UnprocessableEntityError(message);
 		}
 	}
+
+	async parse(schema: ZodSchema, params: any) {
+		try {
+			return schema.parse(params);
+		} catch (error: any) {
+			let message = "Dados de requisição mal formatados";
+			const errors = error.errors;
+
+			for (const error of errors) {
+				if (error.message === "Required") {
+					message =
+						"Dados de requisição mal formatados: um ou mais campos necessários não foram fornecidos.";
+					throw new BadRequestError(message);
+				}
+
+				if (message === "Dados de requisição mal formatados") {
+					message += `: ${error.message};`;
+				} else {
+					message += `${error.message};`;
+				}
+			}
+
+			throw new UnprocessableEntityError(message);
+		}
+	}
 }
 
 export const validateParams = new ValidateParams();
